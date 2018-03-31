60e08f4c40bd1c8ad7f05dce989dace31dbe7564
handsomesnail <zc992833886@outlook.com>
2018-04-01 00:10:34 +0800

    count logic

--- UnitTest/unittest1.cpp
+++ UnitTest/unittest1.cpp
@@ -1,18 +1,132 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <string>
+#include <map>
 
+using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+extern void ReadAndCount(string fileName);
+extern map<string, int> wordMap;
+void PrintMap();
 
 namespace UnitTest
 {		
+
 	TEST_CLASS(UnitTest1)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestMethod1_1)
 		{
-			// TODO: 在此输入测试代码
+			ReadAndCount("data1.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_2)
+		{
+			ReadAndCount("data2.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_3)
+		{
+			ReadAndCount("data3.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_4)
+		{
+			ReadAndCount("data4.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_5)
+		{
+			ReadAndCount("data5.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_6)
+		{
+			ReadAndCount("data6.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_7)
+		{
+			ReadAndCount("data7.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_8)
+		{
+			ReadAndCount("data8.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_9)
+		{
+			ReadAndCount("data9.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_10)
+		{
+			ReadAndCount("data10.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_11)
+		{
+			ReadAndCount("data11.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_12)
+		{
+			ReadAndCount("data12.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_13)
+		{
+			ReadAndCount("data13.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_14)
+		{
+			ReadAndCount("data14.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_15)
+		{
+			ReadAndCount("data15.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_16)
+		{
+			ReadAndCount("data16.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_17)
+		{
+			ReadAndCount("data17.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_18)
+		{
+			ReadAndCount("data18.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_19)
+		{
+			ReadAndCount("data19.txt");
+			PrintMap();
+		}
+		TEST_METHOD(TestMethod1_20)
+		{
+			ReadAndCount("data20.txt");
+			PrintMap();
 		}
 
 	};
+
+}
+
+void PrintMap() {
+	map<string, int>::iterator it;
+	it = wordMap.begin();
+	while (it != wordMap.end()) {
+		Logger::WriteMessage((it->first + " "+to_string(it->second)+"\n").c_str());
+		it++;
+	}
 }
--- WordCountPro/WordCountPro.cpp
+++ WordCountPro/WordCountPro.cpp
@@ -1,4 +1,4 @@
-﻿// WordCountPro.cpp: 定义控制台应用程序的入口点。
+// WordCountPro.cpp: 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
@@ -14,8 +14,9 @@
 
 using namespace std;
 
+
 static set<char> splitSymbolsSet;//记录分隔符
-static map<string, int> wordMap;//记录单词-个数
+map<string, int> wordMap;//记录单词-个数
 static string tempWord = "";//临时存储单词
 static char splitSymbolsArray[43] = { '-', ' ','~','`','!','#','%','^','&','*','(',')', '_','…','[',']','+','=',':',';','\"','\'','|','<','>',',','.','/','?','\r','\n','\0','\t',
 '0','1', '2', '3', '4', '5', '6', '7', '8', '9', };
@@ -30,5 +31,48 @@ string OpenAFile();
 int main(int argc, char *argv[])
 {
     return 0;
+}
+
+//读取文件并计数
+void ReadAndCount(string fileName) {
+	splitSymbolsSet.insert(splitSymbolsArray, splitSymbolsArray + 43);
+	ifstream fin;
+	string tempInput;//存储输入
+
+	fin.open(fileName);
+	while (fin >> tempInput) {
+		for (int i = 0; i < tempInput.length(); i++) {
+			if (tempInput[i] == '-') {
+				if (i > 0 && i < tempInput.length() - 1 && !IsSplitSymbol(tempInput[i - 1]) && !IsSplitSymbol(tempInput[i + 1])) {
+					tempWord += tolower(tempInput[i]);
+				}
+			}
+			else if (!IsSplitSymbol(tempInput[i])) {
+				tempWord += tolower(tempInput[i]);
+			}
+			else AddWord();
+		}
+		AddWord();
+	}
+	fin.close();
+}
+
+//将tempWord传入单词表
+inline void AddWord() {
+	if (tempWord == "")
+		return;
+
+	if (wordMap.find(tempWord) != wordMap.end()) {
+		wordMap[tempWord]++;
+	}
+	else wordMap.insert(pair<string, int>(tempWord, 1));
+	tempWord.clear();
+}
+
+//返回是否是分隔符
+inline bool IsSplitSymbol(char c) {
+	if (splitSymbolsSet.find(c) != splitSymbolsSet.end())
+		return true;
+	return false;
 }
 
