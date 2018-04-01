8a5e2db6b870986a7071c797b50ef791cb345aac
liuqiang666 <1663863667@qq.com>
2018-04-01 19:33:11 +0800

    CmdInput

--- WordCountPro/WordCountPro.cpp
+++ WordCountPro/WordCountPro.cpp
@@ -1,4 +1,4 @@
-﻿// WordCountPro.cpp: 定义控制台应用程序的入口点。
+// WordCountPro.cpp: 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
@@ -17,38 +17,51 @@ using namespace std;
 
 static set<char> splitSymbolsSet;//记录分隔符
 map<string, int> wordMap;//记录单词-个数
-static string tempWord = "";//临时存储单词
-static char splitSymbolsArray[43] = { '-', ' ','~','`','!','#','%','^','&','*','(',')', '_','…','[',']','+','=',':',';','\"','\'','|','<','>',',','.','/','?','\r','\n','\0','\t','0','1', '2', '3', '4', '5', '6', '7', '8', '9', };
+static string tempWord = "";//临时存储单词 ,'…'
+static char splitSymbolsArray[43] = { '-', ' ','~','`','!','#','%','^','&','*','(',')', '_','[',']','+','=',':',';','\"','\'','|','<','>',',','.','/','?','\r','\n','\0','\t','0','1', '2', '3', '4', '5', '6', '7', '8', '9', };
 bool Compare(pair<string, int> p1, pair<string, int> p2);
 void AddWord();
 bool IsSplitSymbol(char c);
 void ReadAndCount(string fileName);
 void Output();
 string OpenAFile();
+string CmdInput(int argc, char *argv[], int* code);
 
 int main(int argc, char *argv[])
+{
+	int code = 0;
+	string path = CmdInput(argc, argv, &code);
+	if (code != 0)
+		return -1;
+	ReadAndCount(path);
+	Output();
+	return 0;
+}
+
+//读取命令行参数
+string CmdInput(int argc, char *argv[], int* code)
 {
 	string path = "";
 	if (argc != 2) {
 		cout << "Please input correct parameter." << endl;
-		return -1;
+		*code = -1;
+		return path;
 	}
 	if (strcmp(argv[1], "-x") == 0) {
 		path = OpenAFile();
 		if (path == "")
-			return -3;
+			*code = -3;
+		return path;
 	}
 	else {
 		path = argv[1];
 		int fileNamelength = strlen(argv[1]);
 		if (strcmp(argv[1] + fileNamelength - 3, "txt") != 0) {
 			cout << "Please input correct parameter." << endl;
-			return -2;
+			*code = -2;
 		}
 	}
-	ReadAndCount(path);
-	Output();
-	return 0;
+	return path;
 }
 
 //读取文件并计数
@@ -117,7 +130,7 @@ string OpenAFile() {
 		char* chRtn = new char[iLen * sizeof(char)];
 		WideCharToMultiByte(CP_ACP, 0, szFile, -1, chRtn, iLen, NULL, NULL);
 		string str(chRtn);
-		delete chRtn;
+		delete[] chRtn;
 		return str;
 	}
 	else {
