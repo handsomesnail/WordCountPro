08e486c2f6c93f37e75710ad46e9e6e4f8c7db69
handsomesnail <zc992833886@outlook.com>
2018-04-01 19:49:02 +0800

    stage2

--- WordCountPro/WordCountPro.cpp
+++ WordCountPro/WordCountPro.cpp
@@ -1,4 +1,4 @@
-// WordCountPro.cpp: 定义控制台应用程序的入口点。
+﻿// WordCountPro.cpp: 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
@@ -18,8 +18,8 @@ using namespace std;
 
 static set<char> splitSymbolsSet;//记录分隔符
 map<string, int> wordMap;//记录单词-个数
-static string tempWord = "";//临时存储单词 ,'…'
-static char splitSymbolsArray[43] = { '-', ' ','~','`','!','#','%','^','&','*','(',')', '_','[',']','+','=',':',';','\"','\'','|','<','>',',','.','/','?','\r','\n','\0','\t','0','1', '2', '3', '4', '5', '6', '7', '8', '9', };
+static string tempWord = "";//临时存储单词 
+static char splitSymbolsArray[43] = { '-', ' ','~','…','`','!','#','%','^','&','*','(',')', '_','[',']','+','=',':',';','\"','\'','|','<','>',',','.','/','?','\r','\n','\0','\t','0','1', '2', '3', '4', '5', '6', '7', '8', '9', };
 bool Compare(pair<string, int> p1, pair<string, int> p2);
 void AddWord();
 bool IsSplitSymbol(char c);
